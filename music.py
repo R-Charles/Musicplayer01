ea2754bf38d86dda57f107c97b933267a369c38f
Raphael Charles <106124732+R-Charles@users.noreply.github.com>
2024-07-16 09:29:52 -0700

    first commit

---
+++ music.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+#colors
+co1 = "#ffffff" #white
+co2 = "#3C1DC6" #purple
+co3 = "#333333" #black
+co4 = "#CFC7F8" #light purple
+
+window = Tk()
+window.title("")
+window.geometry('352x255')
+window.configure(background=co1)
+window.resizable(width=FALSE, height=FALSE)
+
+window.mainloop()
